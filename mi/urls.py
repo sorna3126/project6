51b1d512cbda6fb7cef09f45d22698bb89bda1ad
sorna3126 <sornak3105@gmail.com>
2023-11-15 22:06:29 +0530

    first commit

---
+++ mi/urls.py
@@ -0,0 +1,6 @@
+from mi.views import *
+from django.urls import path
+app_name='rohit'
+urlpatterns=[
+    path('rohit/',rohit,name='rohit'),
+]
---
+++ rcb/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def virat(request):
+    return render(request,'virat.html')
+
+def siraj(request):
+    return HttpResponse('<h1>siraj is a player of RCB</h1>')
